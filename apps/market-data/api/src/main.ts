9e5564696ecabe001208019ccfe7f7bb14383d10
ywarezk <ywarezk@gmail.com>
2026-08-05 18:02:30 +0300

    fix(market-data-api): build and lint fix

--- apps/market-data/api/src/main.ts
+++ apps/market-data/api/src/main.ts
@@ -1,16 +1,16 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
 
 const fastify = Fastify({
   logger: true,
 });
 
-fastify.get("/", async () => {
-  return { message: "Hello World" };
+fastify.get('/', async () => {
+  return { message: 'Hello World' };
 });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
